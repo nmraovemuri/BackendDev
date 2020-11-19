70a5318f6fffe904b09112f26ab5d9b47fc7f616
nmraovemuri <malli.vemuri@gmail.com>
2020-11-19 20:05:44 +0530

    middleware

---
+++ middleware.js
@@ -0,0 +1,26 @@
+ var jwt = require('jsonwebtoken');
+
+ exports.verify= function(req,res,next){
+    console.log("request",req);
+    if(!req.headers.authorization){
+        return res.status(401).send("unauthorized request");
+    }
+    console.log(req.headers.authorization);
+    let token = req.headers.authorization.split(' ')[1];
+    console.log("Token is available:",token);
+    if(token==null)
+    {
+        res.status(401).send("Invalid Token");
+    }
+    jwt.verify(token,'secret_key',(error,payload)=>{
+        console.log("payload :",payload);
+        if(error) 
+          {
+              console.log("message:",error.message);
+              res.status(401).send("Token error message");
+          }
+        else
+        next();
+
+    })
+}
