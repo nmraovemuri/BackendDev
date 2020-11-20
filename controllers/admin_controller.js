cdc03bd69ac9d04f62218489cc3282aba20dece4
dmkjava <dmk.java@gmail.com>
2020-11-20 11:38:46 +0530

    Change Password is added

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -32,3 +32,56 @@ exports.adminSignIn = function (req, res){
         }
     });
 }
+
+// Admin Change Password
+exports.changeAdminPassword = function (req, res){
+    console.log(req.body);
+    const {emailID, oldPassword, newPassword} = req.body
+    if(!emailID || !oldPassword || !newPassword){
+       return res.status(422).json({
+           status: "failed",
+           error:"Please provide emailID, old password and new password"
+        })
+    }
+    if(newPassword.length === 0 || newPassword.length<4)
+        return res.status(422).json({
+            status: "failed",
+            error:"New password is too short"
+        })
+    if(oldPassword === newPassword)
+        return res.status(422).json({
+            status: "failed",
+            error:"While change password,  old password and new password should not be same"
+        })
+    let sql = 'SELECT * from asm_admin where email_id = ? '
+    
+    db.query(sql, [emailID], (err, rows, fields)=>{
+        if(err) 
+            return res.status(422).json({
+                status: "failed",
+                error:"Invalid EmailID "
+            });
+
+        else if(rows.length === 0 || rows[0].password !== oldPassword)
+                return res.status(422).json({
+                    status: "failed",
+                    error:"Invalid EmailID or password"
+                });
+        else{
+            db.query("UPDATE asm_admin SET password = ? where email_id= ? ", 
+                [newPassword, emailID], function (err, rows) {
+                    if(err) 
+                        return res.status(422).json({
+                            status: "failed",
+                            error: err.message
+                        });
+                    console.log(rows);
+                    res.json({ 
+                        status: "success", 
+                        msg: "Password is changed successfully"
+                    });
+            })
+        }
+    });
+}
+
--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jsonwebtoken');
 const adminController = require('../controllers/admin_controller');
 
 //Admin SignIn
 router.post('/adminsignin', adminController.adminSignIn);
 
+//Change Password
+router.post('/change_admin_password', ensureToken, adminController.changeAdminPassword);
+
+// Ensuring Authorization
+function ensureToken(req, res, next){
+    console.log(req.headers);
+    const bearerHeader = req.headers["authorization"];
+    if(typeof bearerHeader !== 'undefined'){
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1]
+        jwt.verify(bearerToken, 'my-secret-key', function (err, data){
+            if(err){
+                res.status(403).json({
+                    status: "failed",
+                    error:"Invalid Authorization"
+                });
+            }
+        });
+        next();
+    }else{
+        return res.status(403).json({
+            status: "failed",
+            error:"Forbidden"
+        });
+    }
+}
 module.exports = router
