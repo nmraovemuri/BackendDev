a47cb202887e74e57eadc42999eabd928875548a
dmkjava <dmk.java@gmail.com>
2020-12-02 14:56:02 +0530

    client access category and subcategory api added

--- routes/category_routes.js
+++ routes/category_routes.js
@@ -6,8 +6,10 @@ const categoryController = require('../controllers/category_controller');
 // Create a new Category 
 router.post('/create_category',  auth.ensureToken, categoryController.createCategory);
 
-// Get All Categories
-router.get('/getAllCategories',  auth.ensureToken, categoryController.getAllCategories);
+// Get All Categories for Admin
+router.get('/admin/getAllCategories',  auth.ensureToken, categoryController.getAllCategories);
 
+// Get All Categories  For Client
+router.get('/client/getAllCategories',  categoryController.getAllCategories);
 
 module.exports = router
--- routes/subcategory_routes.js
+++ routes/subcategory_routes.js
@@ -6,8 +6,11 @@ const categoryController = require('../controllers/subcategory_controller');
 // Create a new SubCategory 
 router.post('/create_subcategory',  auth.ensureToken, categoryController.createSubCategory);
 
-// Get All SubCategories
-router.get('/getAllSubCategories',  auth.ensureToken, categoryController.getAllSubCategories);
+// Get All SubCategories for Admin
+router.get('/admin/getAllSubCategories',  auth.ensureToken, categoryController.getAllSubCategories);
+
+// Get All SubCategories for Clients
+router.get('/client/getAllSubCategories', categoryController.getAllSubCategories);
 
 
 module.exports = router
