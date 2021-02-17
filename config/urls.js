3783d6b9135c877c841e465d181facbd381ebddd
dmkjava <dmk.java@gmail.com>
2021-02-18 01:16:43 +0530

    moving to aws

--- config/urls.js
+++ config/urls.js
@@ -1,9 +1,9 @@
 let urls = {
   CLIENT: process.env.NODE_ENV === 'production'
-  ? 'http://43.241.36.16:3000'
+  ? 'http://3.131.91.97:3000'
   : 'http://localhost:4200',
   SERVER: process.env.NODE_ENV === 'production'
-  ? 'http://43.241.36.16:3000'
+  ? 'http://3.131.91.97:3000'
   : 'http://localhost:3000'
 }
 
