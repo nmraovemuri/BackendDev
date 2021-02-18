c708e143f10c51fd14563b5838ca0ec558eab664
dmkjava <dmk.java@gmail.com>
2021-02-18 15:48:35 +0530

    server ip setup as local application for angular app

--- config/urls.js
+++ config/urls.js
@@ -1,6 +1,6 @@
 let urls = {
   CLIENT: process.env.NODE_ENV === 'production'
-  ? 'http://43.241.36.16:3000'
+  ? 'http://43.241.36.16:4200'
   : 'http://localhost:4200',
   SERVER: process.env.NODE_ENV === 'production'
   ? 'http://43.241.36.16:3000'
