1fb5f792754aefb58c19236a46cd4adf47758edc
dmkjava <dmk.java@gmail.com>
2021-02-18 16:01:20 +0530

    ip changed

--- config/urls.js
+++ config/urls.js
@@ -1,10 +1,10 @@
 let urls = {
   CLIENT: process.env.NODE_ENV === 'production'
   ? 'http://43.241.36.16:4200'
-  : 'http://localhost:4200',
+  : 'http://43.241.36.16:4200',
   SERVER: process.env.NODE_ENV === 'production'
   ? 'http://43.241.36.16:3000'
-  : 'http://localhost:3000'
+  : 'http://43.241.36.16:3000'
 }
 
 
