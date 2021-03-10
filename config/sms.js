eef141ae5002f0b2f4222a1fa37eaed4128f7164
dmkjava <dmk.java@gmail.com>
2021-03-10 13:21:09 +0530

    sdf

--- config/sms.js
+++ config/sms.js
@@ -1,6 +1,6 @@
 const user_id = 'ram_aswika';
 const password = 'ram$$123';
-const sender = 'INVITE';
+const sender = 'ASWIKA';
 
 let sms = {
     user_id : 'ram_aswika',
@@ -10,5 +10,5 @@ let sms = {
     URL: `https://login.bulksmsgateway.in/sendmessage.php?user=${user_id}&password=${password}&sender=${sender}&type=3&template_id=123`
   }
   
-  
+  console.log("sms=", sms);
   module.exports = sms;
