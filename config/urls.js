6982876c4b94d56f53988382f503a6010e1d27b6
dmkjava <dmk.java@gmail.com>
2021-05-20 20:45:34 +0530

    ip is replaced by domain name in urls

--- config/urls.js
+++ config/urls.js
@@ -1,9 +1,9 @@
 let urls = {
   CLIENT: process.env.NODE_ENV === 'production'
-  ? 'http://43.241.36.16:3000'
+  ? 'https://aswikamart.com'
   : 'http://localhost:4200',
   SERVER: process.env.NODE_ENV === 'production'
-  ? 'http://43.241.36.16:3000'
+  ? 'https://aswikamart.com'
   : 'http://localhost:3000',
   ADMIN_CLIENT: process.env.NODE_ENV === 'production'
   ? 'http://43.241.36.16:3306'
